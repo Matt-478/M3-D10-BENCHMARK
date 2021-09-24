82deeb086e72d18f6c0b6e70669913beaddaa55b
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-24 14:22:23 +0300

    Created files, created HTML and CSS
    Hardcorded

---
+++ striveflix.js
@@ -0,0 +1,23 @@
+
+// const getData = async() => {
+//   const url = "https://striveschool-api.herokuapp.com/api/movies"
+
+//   const response = await fetch(url, {
+//     "method": "GET",
+//     "headers": {
+//       "Authorizaton": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRjNzc2NmZkZGUyZjAwMTVlZjQ5NjUiLCJpYXQiOjE2MzI0MDEyNTQsImV4cCI6MTYzMzYxMDg1NH0.VMukG9Igd9jOg79SsFCMjaiM1E8IS9mBxfixDgDdhgM"
+//     }
+//   })
+//   const jsonData = await response.json()
+
+//   console.log("DATA: ", jsonData)
+
+//   // .then(body => console.log(body))
+//   // .catch(err=> console.log(err))
+// }
+
+
+// window.onload = () => {
+//   console.log("Hi")
+//   getData()
+// }
