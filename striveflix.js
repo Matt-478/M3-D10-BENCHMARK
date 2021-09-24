46d1621a48ff4e07e2baa4641b01989ea9daa7ef
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-24 15:29:24 +0300

    GET request done

--- striveflix.js
+++ striveflix.js
@@ -1,23 +1,20 @@
 
-const getData = async() => {
-  const url = "https://striveschool-api.herokuapp.com/api/movies"
-
-  const response = await fetch(url, {
-    "method": "GET",
-    "headers": {
-      "Authorizaton": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRjNzc2NmZkZGUyZjAwMTVlZjQ5NjUiLCJpYXQiOjE2MzI0MDEyNTQsImV4cCI6MTYzMzYxMDg1NH0.VMukG9Igd9jOg79SsFCMjaiM1E8IS9mBxfixDgDdhgM"
-    }
-  })
-  const jsonData = await response.json()
-
-  console.log("DATA: ", jsonData)
-
-  // .then(body => console.log(body))
-  // .catch(err=> console.log(err))
-}
-
-
 window.onload = () => {
-  console.log("Hi")
-  getData()
+  
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRjNzc2NmZkZGUyZjAwMTVlZjQ5NjUiLCJpYXQiOjE2MzI0MDEyNTQsImV4cCI6MTYzMzYxMDg1NH0.VMukG9Igd9jOg79SsFCMjaiM1E8IS9mBxfixDgDdhgM ");
+
+
+var requestOptions = {
+  "method": 'GET',
+  "headers": myHeaders,
+  "redirect": 'follow'
+};
+
+fetch("https://striveschool-api.herokuapp.com/api/movies", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error))
+
+
 }
